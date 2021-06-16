23d6ddec3ce0685c340e74b8a111ba22064a3a9b
homecat805 <13050082@qq.com>
2021-06-16 21:50:49 +0800

    New commit

---
+++ js/main.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+
+	// Navigation - Main
+	$("#menu-toggle").click(function () {
+		$("#menu-content").slideToggle(250);
+	});
+	// Navigation - Main End
+
+	// Carousel
+	$(".carousel-control-right").click(function () {
+		$(".carousel-content").children(".active").fadeOut(1000);
+		if ( $(".carousel-content").children(".active").next().length > 0 ){
+			$(".carousel-content").children(".active").next()
+				.fadeIn(1000)
+				.addClass("active");
+			$(".carousel-content").children(".active").first().removeClass("active");
+		}
+		else {
+			$(".carousel-content").children(".carousel-content-item").first()
+				.fadeIn(1000)
+				.addClass("active");
+			$(".carousel-content").children(".active").last().removeClass("active");
+		}
+	})
+
+	$(".carousel-control-left").click(function () {
+		$(".carousel-content").children(".active").fadeOut(1000);
+		if ( $(".carousel-content").children(".active").prev().length > 0 ){
+			$(".carousel-content").children(".active").prev()
+				.fadeIn(1000)
+				.addClass("active");
+			$(".carousel-content").children(".active").last().removeClass("active");
+		}
+		else {
+			$(".carousel-content").children(".carousel-content-item").last()
+				.fadeIn(1000)
+				.addClass("active");
+			$(".carousel-content").children(".active").first().removeClass("active");
+		}
+	});
+	// Carousel End
+
+});
